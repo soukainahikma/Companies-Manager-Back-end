2b7b75c6c42edf9c599940cfeab77f8db7028f5b
Soukaina Hikma <soukainahikma@gmail.com>
2022-01-31 12:39:27 +0100

    fixing the Users

--- src/db.ts
+++ src/db.ts
@@ -1,3 +1,12 @@
+import {v4 as uuid} from "uuid"
 export let companies = []
 
-export let users = []
+export let users = [
+    {
+        id: uuid(),
+        name: 'admin',
+        email: 'admin@admin.com',
+        password: 'admin',
+        role: 'admin'
+    }
+]
